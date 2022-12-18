ae07f800009ccd0da2fc04d59388e41d4ad6f741
mvd7 <118620242+mvd7@users.noreply.github.com>
2022-12-18 02:16:46 +0100

    Homework - 3 Finished
    Homework - 3 Finished

---
+++ Javascript Homework - 3/app.js
@@ -0,0 +1,11 @@
+console.log("working script");
+
+function typeOfFunction(insert){
+    console.log(typeof insert);
+}
+
+typeOfFunction({});
+typeOfFunction(true);
+typeOfFunction(7);
+typeOfFunction("string");
+typeOfFunction();
