91d01ef0f7106abae73d0a818e85360d601c9ec2
mvd7 <118620242+mvd7@users.noreply.github.com>
2022-12-18 03:39:39 +0100

    Homework 9 - Finished
    Homework 9 - Finished

---
+++ Javascript Homework - 9/app.js
@@ -0,0 +1,10 @@
+console.log("working script");
+
+for(let i=1; i<= 20; i++){
+    if(i % 2){
+        console.log(`${i} number is odd`)
+    }
+    else{
+        console.log(`${i} number is even`)
+    }
+}
