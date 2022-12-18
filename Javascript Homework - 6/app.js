524ffa63e06730d787dca74dc84731c126c67082
mvd7 <118620242+mvd7@users.noreply.github.com>
2022-12-18 02:50:07 +0100

    Homework - 6 Finished
    Homework - 6 Finished

---
+++ Javascript Homework - 6/app.js
@@ -0,0 +1,7 @@
+console.log("hello");
+
+function tellStory(name,mood,activity){
+    console.log(`This is ${name}. ${name} is a nice person.Today they are ${mood}.They are ${activity} all day. The end.`)
+}
+
+tellStory("Tomce","happy","learning");
