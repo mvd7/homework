e64b16255925ee85f51112858aea8fda6cf80eb9
mvd7 <118620242+mvd7@users.noreply.github.com>
2022-12-18 03:59:42 +0100

    Homework 11 - Finished
    Homework 11 - Finished

---
+++ Javascript Homework - 11/app.js
@@ -0,0 +1,9 @@
+console.log("working script");
+
+const firstName = ["Bob", "Jill"];
+const lastName = ["Gregory", "Wurtz"];
+const resultArr = [];
+
+for(let i=0; i < firstName.length; i++){
+    console.log(`${i+1}. ${firstName[i]} ${lastName[i]}`)
+}
