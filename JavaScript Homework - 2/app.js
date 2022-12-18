13abcf26a7c4ccdcb578bc08b7abc3fd7560451d
mvd7 <118620242+mvd7@users.noreply.github.com>
2022-12-18 01:57:42 +0100

    Homework - 2 Finished
    Homework - 2 Finished

---
+++ JavaScript Homework - 2/app.js
@@ -0,0 +1,41 @@
+console.log("hi");
+
+let userYear = prompt("Enter year:")
+let chineseYear = (userYear - 4) % 12;
+
+if (chineseYear === 0) {
+    console.log("Rat");
+}
+if (chineseYear === 1) {
+    console.log("Ox");
+}
+if (chineseYear === 2) {
+    console.log("Tiger");
+}
+if (chineseYear === 3) {
+    console.log("Rabbit");
+}
+if (chineseYear === 4) {
+    console.log("Dragon");
+}
+if (chineseYear === 5) {
+    console.log("Snake");
+}
+if (chineseYear === 6) {
+    console.log("Horse");
+}
+if (chineseYear === 7) {
+    console.log("Goat");
+}
+if (chineseYear === 8) {
+    console.log("Monkey");
+}
+if (chineseYear === 9) {
+    console.log("Rooster");
+}
+if (chineseYear === 10) {
+    console.log("Dog");
+}
+if (chineseYear === 11) {
+    console.log("Pig");
+}
